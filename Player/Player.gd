4be5107b602e9eedc2c4f6c82c7748eeb81d9508
Vitor Augusto Silva <vitor@icarustech.com.br>
2021-01-20 20:25:16 -0300

    add player/bush collision

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const ACCELERATION = 50
-const MAX_SPEED = 150
-const FRICTION = 25
+const ACCELERATION = 500
+const MAX_SPEED = 100
+const FRICTION = 500
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
@@ -14,13 +14,14 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELERATION * delta 
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) 
 	print (velocity)
-	# It is being multiplied by delta so the frame rate doesn't affect the velocity
-	move_and_collide(velocity)
+	# Move and slide multiplies by delta by default
+	# Also, just receiving the move_and_slide return makes some weird behavior not happen anymore when coliding
+	velocity = move_and_slide(velocity)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
