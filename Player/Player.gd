0f870e914eb943c8b5f17269c8aadf7badf5472f
Vitor Augusto Silva <vitor@icarustech.com.br>
2021-01-21 00:12:21 -0300

    Refactor bush/player nodes and add player idle/run animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,22 +2,32 @@ extends KinematicBody2D
 
 const ACCELERATION = 500
 const MAX_SPEED = 100
-const FRICTION = 500
+const FRICTION = 700
+
 var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	# everything that  changes overtime you need to multiply it by delta
 	# so it doesn't vary on the users' framerate
+	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) 
-	print (velocity)
+	print(velocity)
 	# Move and slide multiplies by delta by default
 	# Also, just receiving the move_and_slide return makes some weird behavior not happen anymore when coliding
 	velocity = move_and_slide(velocity)
